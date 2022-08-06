d19558cbf5fb863e66654c2b72f2dcaf67399095
Herberth Andrade <andrade.engmecanica@gmail.com>
2022-08-06 18:43:46 -0300

    novas modificações

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
@@ -22,39 +23,49 @@ const App = () => {
 
   const handleTaskClick = (taskId) => {
     const newTasks = tasks.map((task) => {
-      if (task.id === taskId) return {...task, completed: !task.completed};
+      if (task.id === taskId) return { ...task, completed: !task.completed };
       return task;
     });
     setTasks(newTasks);
-  }
+  };
 
   const handleTaskAddition = (taskTitle) => {
-    const newTasks = [...tasks,
+    const newTasks = [
+      ...tasks,
       {
-      title: taskTitle,
-      id: uuidv4(),
-      completed: false,
-    }];
+        title: taskTitle,
+        id: uuidv4(),
+        completed: false,
+      },
+    ];
     setTasks(newTasks);
-  }
+  };
 
   const handleTaskDeletion = (taskId) => {
-    const newTasks = tasks.filter(task => task.id !== taskId)
-    setTasks(newTasks)
-  }
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  };
 
   return (
-    <>
+    <Router>
       <div className="container">
         <Header />
-        <AddTask handleTaskAddition={handleTaskAddition}/>
-        <Tasks
-        tasks={tasks}
-        handleTaskClick={handleTaskClick}
-        handleTaskDeletion={handleTaskDeletion}
-        />
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <>
+                <AddTask handleTaskAddition={handleTaskAddition} />
+                <Tasks
+                  tasks={tasks}
+                  handleTaskClick={handleTaskClick}
+                  handleTaskDeletion={handleTaskDeletion}
+                />
+              </>
+            )}
+          />
       </div>
-    </>
+    </Router>
   );
 };
 
